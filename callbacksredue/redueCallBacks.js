ecf8c08278f7957639e35d23a09d1628ea27297c
Tim Garlick <timgarlick11@gmail.com>
2015-01-21 14:12:24 -0700

    30% done trying to get a better understanding

---
+++ callbacksredue/redueCallBacks.js
@@ -0,0 +1,169 @@
+/* In this repo your job is to write functions to make each function call work properly.
+Below is a sample problem 
+  //code here for sayHi
+   sayHi('Hi Katie', function(thingToSay){
+      alert(thingToSay);
+   });
+   
+and what you should write is the favNum function that makes the code above work, 
+    
+    
+   var sayHi = function(str, cb){
+    cb(str);
+   }
+   sayHi('Hi Katie', function(thingToSay){
+      alert(thingToSay); //should alert ('Hi Katie')'
+   });
+    
+    
+*/
+
+
+
+  //Code Here for first
+  
+var names = ['Tyler', 'Cahlan', 'Ryan', 'Colt', 'Tyler', 'Blaine', 'Cahlan'];
+
+var first = function(person, callBack) {
+  callBack(person[0]);
+}
+
+first(names, function(firstName){
+  console.log('The first name in names is ', firstName)
+});
+
+
+
+/* NEXT PROBLEM ---- NEXT PROBLEM ---- NEXT PROBLEM ---- NEXT PROBLEM */
+
+
+
+
+  //Code Here for last
+
+var names = ['Tyler', 'Cahlan', 'Ryan', 'Colt', 'Tyler', 'Blaine', 'Cahlan'];
+
+var last = function(person, callBack) {
+  callBack(person[person.length -1]); // what is in the [] is the value at that index not the index number
+}
+last(names, function(lastName){
+  console.log('The last name in names is ', lastName);
+});
+
+
+
+
+
+/* NEXT PROBLEM ---- NEXT PROBLEM ---- NEXT PROBLEM ---- NEXT PROBLEM */
+
+
+var multiply = function(num1, num2, callback) {
+  var result = num1 * num2;
+  callback(result);
+}
+
+
+multiply(4, 3, function(answer){
+  console.log('The answer is ', answer); //should console.log 12
+})
+
+
+
+
+
+/* NEXT PROBLEM ---- NEXT PROBLEM ---- NEXT PROBLEM ---- NEXT PROBLEM */
+
+
+
+
+
+  //Code Here for contains
+
+var names = ['Tyler', 'Cahlan', 'Ryan', 'Colt', 'Tyler', 'Blaine', 'Cahlan'];
+
+var contains = function(arr, person, callBack) {
+for (var i = 0; i < arr.length; i++) {
+  if (arr[i] === person) {
+    callBack(true);// if you dont git return you will get that colt is in the array and colt is not in the array. return statements end loops after the condition you were looking for is satisfied
+    }
+  }
+    callBack(false); // this needs to be declared so another value can be passed in if the 'if' statment above return is not satisfied
+}
+
+
+contains(names, 'Colt', function(result){
+  if(result === true){
+    console.log('Colt is in the array');
+  } else {
+    console.log('Colt is not in the array');
+  }
+});
+
+
+
+
+
+/* NEXT PROBLEM ---- NEXT PROBLEM ---- NEXT PROBLEM ---- NEXT PROBLEM */
+
+
+
+
+    //Code Here for uniq
+
+var names = ['Tyler', 'Cahlan', 'Ryan', 'Colt', 'Tyler', 'Blaine', 'Cahlan'];
+uniq(names, function(uniqArr){
+  console.log('The new names array with all the duplicate items removed is ', uniqArr);
+});
+
+
+
+
+
+/* NEXT PROBLEM ---- NEXT PROBLEM ---- NEXT PROBLEM ---- NEXT PROBLEM */
+
+
+
+
+    //Code Here for each
+
+var names = ['Tyler', 'Cahlan', 'Ryan', 'Colt', 'Tyler', 'Blaine', 'Cahlan'];
+each(names, function(item, indice){
+  console.log('The item in the ' + indice + ' position is ' + item)
+});
+
+
+
+
+
+/* NEXT PROBLEM ---- NEXT PROBLEM ---- NEXT PROBLEM ---- NEXT PROBLEM */
+
+
+
+
+
+ //code here for getUserById
+
+var users = [
+  {
+    id: '12d',
+    email: 'tyler@gmail.com',
+    name: 'Tyler',
+    address: '167 East 500 North'
+  },
+  {
+    id: '15a',
+    email: 'cahlan@gmail.com',
+    name: 'Cahlan',
+    address: '135 East 320 North'
+  },
+  {
+    id: '16t',
+    email: 'ryan@gmail.com',
+    name: 'Ryan',
+    address: '192 East 32 North'
+  },
+];
+
+getUserById(users, '16t', function(user){
+  console.log('The user with the id 16t has the email of ' + user.email + ' the name of ' + user.name + ' and the address of ' + user.address); 
+});
